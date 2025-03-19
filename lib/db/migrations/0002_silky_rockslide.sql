52da7ed1b3f581f1ba3cdf6fdc2535a6a85cf99b
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2025-03-20 00:05:01 +0100

    reverted back to previous db migration

--- lib/db/migrations/0002_silky_rockslide.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "MedicalRecord" ALTER COLUMN "diagnosis" SET DATA TYPE varchar;--> statement-breakpoint
-ALTER TABLE "MedicalRecord" ALTER COLUMN "treatment" SET DATA TYPE varchar;--> statement-breakpoint
-ALTER TABLE "MedicalRecord" ALTER COLUMN "prescription" SET DATA TYPE varchar;--> statement-breakpoint
-ALTER TABLE "MedicalRecord" ALTER COLUMN "notes" SET DATA TYPE varchar;
