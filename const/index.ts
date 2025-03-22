bfb79eb975c0252559b893b583ae4b6019cbff79
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2025-03-23 00:18:13 +0100

    disable connecting to server sock

--- const/index.ts
+++ const/index.ts
@@ -1,12 +1,11 @@
 // Function to dynamically determine WebSocket URL based on current environment
 export const getSocketPath = () => {
   // In browser environment
-  if (typeof window !== "undefined") {
-    const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
-    const host = window.location.host; // Includes hostname and port if present
-    console.log("sock path calc", `${protocol}//${host}/ws`);
-    return `${protocol}//${host}/ws`;
-  }
+  // if (typeof window !== "undefined") {
+  //   const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
+  //   const host = window.location.host; // Includes hostname and port if present
+  //   return `${protocol}//${host}/ws`;
+  // }
   // Fallback for server-side rendering
   return "ws://localhost:3000/ws";
 };
